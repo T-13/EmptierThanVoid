e75c5df867b8ca181b1a65d748528c9011ba9545
Planer <nejcplaner@gmail.com>
2017-11-05 20:13:06 +0100

    ETVShip abstract class with two subclasses ETVShipFighter and ETVShipCapital

---
+++ Source/ETV/ETVShip.h
@@ -0,0 +1,77 @@
+﻿// Copyright (C) Team13. All rights reserved.
+
+#pragma once
+
+#include "ETVAction.h"
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "ETVShip.generated.h"
+
+/**
+ * Abstract base Ship class.
+ */
+UCLASS(NotPlaceable, Abstract)
+class ETV_API AETVShip : public APaperSpriteActor
+{
+	GENERATED_BODY()
+	
+
+public:
+	// Sets default values for this actor's properties
+	//TODO AETVShip();
+
+protected:
+	//Name of the ship
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship")
+	FName Name;
+
+	//Health points of the ship
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "0.0"))
+	int32 HealthPoints;
+	
+	//Bonus to health for ships
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "0.0"))
+	int32 ShieldPoints;
+
+	//Time for the shields to recharge
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "0.0"))
+	int32 ShieldRechargeTime;
+
+	//Weapon slots
+	//TODO When AETVWeapon is implemented
+
+	//Integers (x,y) that represent a point on a sprite where the attachement goes
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship")
+	TArray<int32> AttachementSlots;
+
+	//Size of the ship
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "1.0"))
+	int32 Size;
+
+	//Area that ship is allowed to move to
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "1.0"))
+	int32 MoveRange;
+
+	//Integers (x,y) for possible fields that ship can move to
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship")
+	TArray<int32> Fields;
+
+	//Action avaible on ship
+	TArray<UETVAction> Actions;
+
+	//How fast ship moves
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ETV Ship", meta = (ClampMin = "1.0"))
+	int32 ShipSpeed;
+
+
+public:
+	UFUNCTION()
+	virtual void getCurrentPosition();
+
+	UFUNCTION()
+	virtual void RechargeShields();
+
+	UFUNCTION()
+	virtual void getReport();
+
+};
